58a21ad9c1e284c76c0f96f7294ca9d88fdcc91a
wasi131 <wasimullahfarooqi@gmail.com>
2025-08-06 16:13:28 +0300

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav style={{ 
+      padding: '1rem',
+      background: '#2563eb',
+      color: 'white',
+      marginBottom: '2rem'
+    }}>
+      <h3 style={{ margin: 0 }}>Currency System</h3>
+    </nav>
+  )
+}
