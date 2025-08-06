0a4774ae19bbcbfa2a21bcde5499d5de1a76aec6
wasi131 <wasimullahfarooqi@gmail.com>
2025-08-06 15:49:18 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Currency System</h1>
-      <p>Foreign Exchange Management Portal</p>
+      <h1>Foreign Currency System</h1>
+      <p>POS Rate Management Portal</p>
     </div>
   )
 }
