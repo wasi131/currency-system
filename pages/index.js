f269f6c742aef7fe9e2163ab2b156e1bfcfc1ba6
wasi131 <wasimullahfarooqi@gmail.com>
2025-08-06 16:15:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Foreign Currency System</h1>
-      <p>POS Rate Management Portal</p>
+    <div>
+      <Navbar />
+      <div style={{ padding: '0 2rem' }}>
+        <h1>Foreign Currency Rates</h1>
+        <p>Current POS Rates: 1 USD = 3.75 SAR</p>
+      </div>
     </div>
   )
 }
