7973caa963e107fe90e10e7c783350bf3fa5ac90
wasi131 <wasimullahfarooqi@gmail.com>
2025-08-06 16:21:22 +0300

    Create request-rate.js

---
+++ pages/request-rate.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+import RateForm from '../components/RateForm'
+
+export default function RequestRate() {
+  return (
+    <div>
+      <Navbar />
+      <RateForm />
+    </div>
+  )
+}
